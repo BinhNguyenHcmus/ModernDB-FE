a57f409f385a8854981e6be231ca34f6affbeab3
BinhNguyen <ngvanbinh2001@gmail.com>
2023-05-02 00:16:21 +0700

    Init source

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button, DatePicker, Space } from 'antd';
+
+import './App.css';
+
+function App() {
+  return (
+    <Space>
+      <DatePicker />
+      <Button type='primary'>Primary Button</Button>
+    </Space>
+  );
+}
+
+export default App;
